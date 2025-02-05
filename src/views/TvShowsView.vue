ab8d87ea00cbaf478e2839d72b7cdcde77c0ddfb
Neil Sanchez <neil.sanchez@bodytechcorp.com>
2025-02-04 19:09:21 -0500

    add signin and signup views

--- src/views/TvShowsView.vue
+++ src/views/TvShowsView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import ShowsTv from '@/components/ShowsTv.vue'
+import NavbarLayout from '@/views/layouts/NavbarLayout.vue'
 </script>
 
 <template>
+  <NavbarLayout />
   <ShowsTv />
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@ export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "var(--color-primary, #E50914)", // Azul 500 como fallback
+          50: "var(--color-primary-50, #FCAAAE)",
+          100: "var(--color-primary-100, #FB969B)",
+          200: "var(--color-primary-200, #F96F76)",
+          300: "var(--color-primary-300, #F84851)",
+          400: "var(--color-primary-400, #F6212B)",
+          500: "var(--color-primary-500, #E50914)", // Fallback si la variable no está definida
+          600: "var(--color-primary-600, #AF070F)",
+          700: "var(--color-primary-700, #79050B)",
+          800: "var(--color-primary-800, #430306)",
+          900: "var(--color-primary-900, #0D0101)",
+        },
+      }
+    },
   },
   plugins: [],
 }
